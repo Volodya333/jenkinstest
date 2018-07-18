30439b7d67d1dcd9c019ab5c55828a58c04ddee0
Volodya333 <39828789+Volodya333@users.noreply.github.com>
2018-07-18 11:00:30 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@ def sum(num):
     return num + 3
 
 def test_sum():
-    assert sum(2) == 3
+    assert sum(2) == 5
 
 
 
@@ -11,7 +11,7 @@ def dev(num):
     return num/2
 
 def test_dev():
-    assert dev(4) == 9
+    assert dev(4) == 2
 
 
 
